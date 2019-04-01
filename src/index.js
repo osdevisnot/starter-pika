0296de9e09ed8ffcab547e9c6815461ea8ab44cf
osdevisnot <osdevisnot@gmail.com>
2019-04-01 15:01:32 -0700

    improve starter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-document.getElementById('app').innerHTML = `
-<h1>Hello Pika!</h1>
-<div>
-  Look
-  <a href="https://github.com/pikapkg/web" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Pika!
-</div>
-<div>
-  Look
-  <a href="https://github.com/lukejacksonn/servor" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about servor!
-</div>
-`
+import { html, render } from './web_modules/lit-html.js'
+
+const Hello = () => {
+  return html`
+    <h1>Hello Pika!</h1>
+    <div>
+      Look
+      <a href="https://github.com/pikapkg/web" target="_blank" rel="noopener noreferrer">here</a>
+      for more info about Pika!
+    </div>
+    <div>
+      Look
+      <a href="https://github.com/lukejacksonn/servor" target="_blank" rel="noopener noreferrer">here</a>
+      for more info about servor!
+    </div>
+  `
+}
+
+render(Hello(), document.querySelector('#app'))
